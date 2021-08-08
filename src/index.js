6788385af77ff48c548713c89c4848f0f4d759cf
gyanchandra moirangthem <gyanchandra moirangthem>
2021-08-08 18:54:22 +0530

    17. setting up vs code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react'
+import ReactDom from 'react-dom'
 
 function Greeting() {
-  return <h4>this is gyan, welcome home</h4>;
+  return (
+    <>
+      <h4>this is gyan, welcome home</h4>
+    </>
+  )
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+ReactDom.render(<Greeting />, document.getElementById('root'))
